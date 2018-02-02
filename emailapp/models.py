c2033f8965b22890b3f483cc964a180e5c923b87
shankar_swamy <shankarswamy294@gmail.com>
2018-02-02 21:02:16 +0530

    update

--- emailapp/models.py
+++ emailapp/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
-    name=models.CharField(max_length=10)
+    name=models.CharField(max_length=30)
     email=models.EmailField()
 
     def __str__(self):
